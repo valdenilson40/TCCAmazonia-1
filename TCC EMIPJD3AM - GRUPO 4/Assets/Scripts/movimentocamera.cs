ec7f5af45382704af98d6fca39d8a20fbcd79d77
Inácio <inacioviana7@outlook.com>
2022-08-16 14:43:13 -0300

    upd
    hud,vida

--- TCC EMIPJD3AM - GRUPO 4/Assets/Scripts/movimentocamera.cs
+++ TCC EMIPJD3AM - GRUPO 4/Assets/Scripts/movimentocamera.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movimentocamera : MonoBehaviour
 {
     public float velocidadeCamera;
     private Camera cameraPrincial;
     private float cameraY;
+    public float vidaInicial;
+    public float vidaAtual;
+    public float porcentVida;
+    public Image imgVida;
+
 
     void Start()
     {
-        cameraPrincial = Camera.main;  
+        cameraPrincial = Camera.main;
+        vidaAtual = vidaInicial;
     }
 
     private void FixedUpdate() {
@@ -20,5 +27,21 @@ public class movimentocamera : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "vida")
+        {
+            vidaAtual -= 1;
+            porcentVida = vidaAtual / vidaInicial;
+            imgVida.fillAmount = porcentVida;
+        }
+
+        if (other.gameObject.tag == "ganharVida")
+        {
+            vidaAtual = vidaInicial;
+            porcentVida = vidaAtual / vidaInicial;
+            imgVida.fillAmount = porcentVida;
+        }
     }
 }
